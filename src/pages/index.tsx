8e69b5e5c30bde9aaa2b3a0d55e8ab14bc33d074
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2023-04-28 23:07:45 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,6 @@
 import * as React from "react"
 
 const IndexPage = () => (
-
-    <ul>
-      <li>Typescript</li>
-      <li>Material-ui</li>
-      <li>Redux toolkit</li>
-      <li>Dark Theme</li>
-      <li>dotenv</li>
-    </ul>
     <p>In order to change the title, description and other details go to gatsby-config file and edit the title</p>
   
 )
